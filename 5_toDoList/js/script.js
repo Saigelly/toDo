511a421635ee5ce43f8ad650a1be2aa8c94cfe92
Molchanov Dmitriy <Saigelly@gmail.com>
2024-10-18 12:16:36 +0300

    toDoList v0.1.2.1 add few function for panel of control

--- 5_toDoList/js/script.js
+++ 5_toDoList/js/script.js
@@ -9,50 +9,139 @@
 
 // 6) Если хотябы один чекбокс активен, рядом с кнопкой выбрать все появить кнопку "отменить выбор"
 
-const TASK_LIST = document.querySelector('.task__list.task-list')
+// 7) Менять иконку после нажатия кнопки done на какую-то типо отменить
+// const TASK_LIST = document.querySelector('.task-list__tasks') 
+// const TASK_LIST_HEADER = document.querySelector('.task-list__header')
+
+const TASK_LIST = document.querySelector('.task__list')
+let TASK_ID = 0;
+// let TASK_LIST_HEADER_IS_VISIBILITY = false;
 
 
 
-
-// 1)Реализовать создание тасков
-//      при нажатие на кнопку "Добавить"
-//      если что-то есть в инпуте, нарисовать карточку таска с текстом из инпута
+//создание карточки таска при нажатие на кнопку добавить или интер
 const addBtn = document.querySelector('.control__btn');
 
 addBtn.addEventListener('click', () => {
     const taskInput = document.querySelector('.control__input')
-    if(taskInput.value){
+    if (taskInput.value) {
         const newTask = document.createElement('li');
-        newTask.classList.add('task-list__item');
+        newTask.classList.add('task-list__item', 'item');
+
+        const taskID = generateTaskID();
+
+        newTask.dataset.taskId = `task_${taskID}`
         newTask.innerHTML = `
-            <p class="task-list__text">${taskInput.value}</p>
-                            <ul class="task-list__action">
-                                <li class="task-list__btn task-list__btn_done">
-                                    <img src="../5_toDoList/img/done.png"  alt="done" class="task-list__btn-done_img">
+            <input type="checkbox" name="chec-task" class="item__checkbox" id="task_${taskID}" >
+            <p class="item__text">${taskInput.value}</p>
+                            <ul class="item__action">
+                                <li class="btn-action btn-action_done">
+                                    <img src="../5_toDoList/img/done.png"  alt="done" class="btn-action__img btn-action__img_done">
                                 </li>
-                                <li class="task-list__btn task-list__btn_del">
-                                    <img src="../5_toDoList/img/delete.png" alt="delete" class="task-list__btn-del_img">
+                                <li class="btn-action btn-action_del">
+                                    <img src="../5_toDoList/img/delete.png" alt="delete" class="btn-action__img btn-action__img_del">
                                 </li>
                             </ul>
         `;
-        
+
         TASK_LIST.append(newTask);
         taskInput.value = '';
     }
 })
 
-// 2)Сделать логику при нажатие на кнопку done
-//      зачеркнуть текст, поменять фон блоку, поставить галочку
-// 3)Сделать логику при нажатие кнопки delete
-//      удалить карточку
 
-
+//обработка нажатий на карточку таска.
 TASK_LIST.addEventListener('click', (e) => {
     const pressBtn = e.target;
-     if (pressBtn.classList.contains('task-list__btn-done_img')){
-        pressBtn.closest('.task-list__item').classList.toggle('task-list__item_done');
+    //если кнопка выполнить, то меняем стиль таска на "выполнено"
+    if (pressBtn.classList.contains('btn-action__img_done')) {
+        pressBtn.closest('.task-list__item').classList.toggle('item_done');
     }
-    if (pressBtn.classList.contains('task-list__btn-del_img')){
+    //если удалить то удаляем
+    if (pressBtn.classList.contains('btn-action__img_del')) {
         pressBtn.closest('.task-list__item').outerHTML = '';
     }
-} )
+    //если кликнули на другие элементы Таска
+    if (pressBtn.classList.contains('item__action') ||
+        pressBtn.classList.contains('item__text') ||
+        pressBtn.classList.contains('task-list__item')) {
+
+        //записываем нажатый Таск в переменную
+        const checkedTaskEl = pressBtn.closest('.task-list__item');
+        //активируем чекбокс с подходящим id
+        chekboxChecked(checkedTaskEl.dataset.taskId);
+        //меняем стиль таск на "выбранный"
+        checkedTaskEl.classList.toggle('item_checked');
+
+        //передаем количество выбранных тасков
+        updateQuantitySelected();
+        
+        //активируем верхнее меню управление
+        activeTaskListHeader();
+    }
+    //если нажали на cancel отменить выбор
+    // if(pressBtn.classList.contains('btn-action__img_cancel')){
+    //     const checkedCollection = document.querySelectorAll('.item_checked');
+    //     checkedCollection.forEach(el => el.classList.remove('item_checked'));
+    // }
+})
+
+// реализовать алгоритм работы кнопок
+// починить баг, при котором если удалить все таски и один из них был выбранный, то менюшка остается, а должна исчезать
+
+//фнукиця для генерации id для Task
+function generateTaskID() {
+    return TASK_ID += 1;
+}
+
+//функция переключения чекбокса
+function chekboxChecked(id) {
+    const checkBox = document.getElementById(`${id}`);
+    checkBox.checked
+        ? checkBox.checked = false
+        : checkBox.checked = true
+}
+
+
+function updateQuantitySelected() {
+    const checkboxCollection = Array.from(document.querySelectorAll('.item__checkbox'));
+
+    const quntiteActiveEl = document.querySelector('.action-list__quntite');
+        const quntActive = checkboxCollection.reduce((quant, el) => {
+            if (el.checked) {
+                quant += 1;
+            }
+            return quant
+        }
+            , 0);
+    
+        quntiteActiveEl.textContent = `Выбрано ${quntActive}`
+}
+
+function activeTaskListHeader(){
+    const checkboxCollection = Array.from(document.querySelectorAll('.item__checkbox'));
+
+    const taskListHeader = document.querySelector('.task-list__header');
+    
+    const isActive = checkboxCollection.find(el => el.checked);
+    if(isActive && !taskListHeader.classList.contains('_active')){
+        taskListHeader.classList.add('_active');
+    }
+    if(!isActive){
+        taskListHeader.classList.remove('_active');
+    }   
+}
+
+
+
+// Обработка нажатий на кнопки из верхенго меню управления
+
+// TASK_LIST_HEADER.addEventListener('click', (e) =>{
+
+//     const pressBtn = e.target;
+
+//     if(pressBtn.classList.contains('btn-action__img_cancel')){
+//         console.log('Yes')
+//     }
+
+// })
